7f2e28ba224f75275ff44ef51e70597ac910c508
DromerJx <141746127+DromerJx@users.noreply.github.com>
2024-02-15 18:19:00 -0600

    1st upload

---
+++ 03-control-flujo/01-if.js
@@ -0,0 +1,8 @@
+// if (condicion){
+//     expresion
+// }
+// Si la exprecion es de una sola linea, no se ponen llaves
+let edad = 15;
+if (edad > 17){
+    console.log('Usuario mayor de edad');
+}
---
+++ 03-control-flujo/02-else.js
@@ -0,0 +1,8 @@
+let edad = 15;
+
+if (edad > 17)
+    console.log('Usuario mayor de edad');
+else if (edad > 13)
+    console.log('Usuario necesita estar acompanado de sus padres')
+else
+    console.log('No puede ingresar');
